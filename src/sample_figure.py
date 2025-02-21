3af9913e47869bda432edb03a2d2936cfad4afd0
foomoon <daniel.j.claxton@gmail.com>
2025-02-21 15:45:31 -0600

    headless support

--- src/sample_figure.py
+++ src/sample_figure.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")  # Use a non-GUI backend
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import matplotlib.patheffects as path_effects
